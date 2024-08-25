df4d0769cfbf0fbe6c5e712e5c926dd62741cf2e
Jessica Kerr <jessitron@honeycomb.io>
2024-08-25 13:52:49 -0500

    fix: on a 401, tell people it wasn't valid

--- scripts/honeycomb-whoami.sh
+++ scripts/honeycomb-whoami.sh
@@ -13,11 +13,18 @@ fi
 response=$(curl -s -H "X-Honeycomb-Team: $HONEYCOMB_API_KEY" \
                  "https://api.honeycomb.io/1/auth")
 
-# Check if the request was successful
+# Check if the curl command executed at all
 if [ $? -ne 0 ]; then
   echo "Error: Failed to fetch data from Honeycomb API."
   exit 1
+fi  
+
+# Check whether $response containst "401"
+if [[ $response == *"401"* ]]; then
+  echo "This Honeycomb API key is not valid: $HONEYCOMB_API_KEY"
+  exit 1
 fi
+
 
 ###
 # this requires 'jq' to parse the json response.
